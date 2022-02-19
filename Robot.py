2bde3813b062fc7a69e9cb8259ff04b352ac3254
WangPeng <wangpeng930204@gmail.com>
2022-02-19 18:19:33 +0100

    Change robot size to smaller Add more shapes of rooms update the collision detect

--- Robot.py
+++ Robot.py
@@ -1,7 +1,6 @@
 import math
 import random
 from copy import copy
-
 import numpy as np
 from pygame.math import Vector2
 
@@ -27,35 +26,51 @@ def right_intersection(point, wall_point1, wall_point2, sensor_start, sensor_end
     return right
 
 
+def get_intersection(point1, point2, point3, point4):
+    new_x, new_y = 10000, 10000
+    a1 = point2[1] - point1[1]
+    b1 = point1[0] - point2[0]
+    c1 = a1 * point1[0] + b1 * point1[1]
+    # Sensor line
+    a2 = point4[1] - point3[1]
+    b2 = point3[0] - point4[0]
+    c2 = a2 * point3[0] + b2 * point3[1]
+    determinant = a1 * b2 - a2 * b1
+    if abs(determinant) > 0.00000001:  # if there is an intersectioin
+        new_x = -(b1 * c2 - b2 * c1) / determinant  # intersection coordinate
+        new_y = (a1 * c2 - a2 * c1) / determinant
+        if abs(round(new_x) - new_x) < 0.00000001:
+            new_x = round(new_x)
+        if abs(round(new_y) - new_y) < 0.00000001:
+            new_y = round(new_y)
+    return new_x, new_y
+
+
 class Robot:
 
-    def __init__(self, outer_wall, inner_wall, size):
+    def __init__(self, walls, initPosition, size):
         self.radius = int(size / 2)
-        self.x, self.y = self.initPosition(outer_wall, inner_wall)
+
+        self.x, self.y = self.initPosition(initPosition)
         self.frontX = self.x + self.radius
         self.frontY = self.y
         self.Vl = 0
         self.Vr = 0
         self.theta = 0
         self.speed = 3
-        self.sensors, _ = self.distanceToSensors(outer_wall, inner_wall)
-        self.safe_distance = 2
+        self.sensors, _ = self.distanceToSensors(walls)
         self.stop = False
         self.last_wall = []
 
-    def initPosition(self, outer_wall, inner_wall):
-        x = random.randint(outer_wall[0][0] + self.radius, outer_wall[2][0] - self.radius)
-        y = random.randint(outer_wall[0][1] + self.radius, outer_wall[2][1] - self.radius)
-        while (inner_wall[0][0] - self.radius < x < inner_wall[2][0] + self.radius) and (
-                inner_wall[0][1] - self.radius < y < inner_wall[2][1] + self.radius):
-            x = random.randint(outer_wall[0][0] + self.radius, outer_wall[2][0] - self.radius)
-            y = random.randint(outer_wall[0][1] + self.radius, outer_wall[2][1] - self.radius)
+    def initPosition(self, position):
+        x = random.randint(position[0], position[0])
+        y = random.randint(position[1], position[1])
         return x, y
 
-    def move(self, movement, delta_t, outer_wall, inner_wall):
+    def move(self, movement, delta_t, walls):
         # Check keys for movement
         # movement = [w, s, o, l, x, t, g]
-
+        test = "Safe"
         if movement[0] == 1 or movement[5] == 1:
             self.Vl += self.speed
         if movement[1] == 1 or movement[6] == 1:
@@ -88,37 +103,29 @@ class Robot:
                     [ICC[0], ICC[1], w * delta_t])).transpose()
                 next_x, next_y, new_theta = result[0], result[1], result[2]
             # update  sensors
-            self.sensors, walls = self.distanceToSensors(outer_wall, inner_wall)
-
-            # detect collision
-            delta_t, collision = self.detectCollision(delta_t, next_x, next_y)
+            self.sensors, walls = self.distanceToSensors(walls)
             # handle collision
-            next_x, next_y = self.handleCollision(next_x, next_y, walls)
+            collision = self.detectCollision(next_x, next_y, walls)
+            if collision:
+                test = "Danger!"
             # Transfer results from the ICC computation
             self.x = next_x
             self.y = next_y
             self.theta = new_theta
             self.frontX, self.frontY = self.rotate(self.theta, self.radius)
 
-        return self.Vl, self.Vr, np.round(np.degrees(self.theta), 2), delta_t
+        return self.Vl, self.Vr, np.round(np.degrees(self.theta), 2), delta_t, test
 
-    def distanceToSensors(self, outer_wall, inner_wall):
+    def distanceToSensors(self, all_walls):
         dist = []
-        walls = []
+        detected_walls = []
         angle = copy(self.theta)
         for i in range(12):
-            min_dist_out_wall, wall_out = self.distance(outer_wall, angle)
-            min_dist_in_wall, wall_in = self.distance(inner_wall, angle)
-            if min_dist_out_wall > min_dist_in_wall:
-                '''if the distance to closer out wall is bigger than the distance 
-                to the closer in wall, then keep the in wall distance'''
-                dist.append(min_dist_in_wall)
-                walls.append(wall_in)
-            else:
-                dist.append(min_dist_out_wall)
-                walls.append(wall_out)
+            min_dist_out_wall, wall_out = self.distance(all_walls, angle)
+            dist.append(min_dist_out_wall)
+            detected_walls.append(wall_out)
             angle += math.pi / 6
-        return dist, walls
+        return dist, detected_walls
 
     def rotate(self, angle, r):
         # Rotate the robot at a certain angle from the x-axis
@@ -126,108 +133,62 @@ class Robot:
         front_y = self.y + np.sin(-angle) * r
         return front_x, front_y
 
-    def distance(self, wall, angle):
+    def distance(self, walls, angle):
         dist = []
         select_wall = []
-        for i in range(len(wall) - 1):
-            # Out wall line
-            point1 = wall[i]
-            point2 = wall[i + 1]
-            a1 = point2[1] - point1[1]
-            b1 = point1[0] - point2[0]
-            c1 = a1 * point1[0] + b1 * point1[1]
-
-            # Sensor line
-            point3 = [self.x, self.y]
-            point4 = Vector2(self.rotate(angle, self.radius))
-            a2 = point4[1] - point3[1]
-            b2 = point3[0] - point4[0]
-
-            c2 = a2 * point3[0] + b2 * point3[1]
-            determinant = a1 * b2 - a2 * b1
-
-            if abs(determinant) > 0.00000001:  # if there is an intersection
-                new_x = -(b1 * c2 - b2 * c1) / determinant  # intersection coordinate
-                new_y = (a1 * c2 - a2 * c1) / determinant
-                if abs(round(new_x) - new_x) < 0.00000001:
-                    new_x = round(new_x)
-                if abs(round(new_y) - new_y) < 0.00000001:
-                    new_y = round(new_y)
-                # make sure intersection is in front of the sensor
+        for wall in walls:
+            for i in range(len(wall) - 1):
+                # Out wall line
+                point1 = wall[i]
+                point2 = wall[i + 1]
+                # Sensor line
+                point3 = [self.x, self.y]
+                point4 = Vector2(self.rotate(angle, self.radius))
+                new_x, new_y = get_intersection(point1, point2, point3, point4)
                 if right_intersection((new_x, new_y), point1, point2, point3, point4):
                     dist.append(math.sqrt((new_x - point4[0]) ** 2 + (new_y - point4[1]) ** 2))
                     select_wall.append([point1, point2])
-        wall = []
+        walls = []
         if len(dist):
             min_dist_out_wall = min(dist)  # CLoser wall to sensor
             wall_index = dist.index(min_dist_out_wall)
-            wall = select_wall[wall_index]
+            walls = select_wall[wall_index]
         else:
             min_dist_out_wall = 1500
-        return min_dist_out_wall, wall
+        return min_dist_out_wall, walls
 
-    def decomposeMovement(self, move_x, move_y, wall):
+    def robot_and_wall(self, move_x, move_y, wall, closest_sensor):
+        collision = False
+        towards_wall = False
+        intersection_on_wall = True
         theta_wall = slope(wall)
         theta_move = slope([[self.x, self.y], [move_x, move_y]])
         theta_wall_move = theta_wall - theta_move
         move_distance = ((move_x - self.x) ** 2 + (move_y - self.y) ** 2) ** 0.5
-        distance_parallel = abs(move_distance * np.cos(theta_wall_move * math.pi / 180))
-        distance_vertical = -abs(move_distance * np.sin(theta_wall_move * math.pi / 180))
-        parallel_x = distance_parallel
-        parallel_y = distance_parallel
-        inters_x, inters_y = self.get_intersection((self.x, self.y), (move_x, move_y), wall[0], wall[1])
+        distance_vertical = abs(move_distance * np.sin(theta_wall_move * math.pi / 180))
+        inters_x, inters_y = get_intersection((self.x, self.y), (move_x, move_y), wall[0], wall[1])
         if (move_x - inters_x) * (move_x - self.x) <= 0 and (move_y - inters_y) * (move_y - self.y) <= 0:
-            distance_vertical = -distance_vertical
-
-        if theta_wall == 90:
-            if move_y < self.y:
-                parallel_x = -distance_parallel
-                parallel_y = -distance_parallel
-        if theta_wall == 0:
-            if move_x < self.x:
-                parallel_x = -distance_parallel
-                parallel_y = -distance_parallel
-        elif theta_wall < 0:
-            if abs(theta_move) < 90:
-                parallel_y = -distance_parallel
+            towards_wall = True
+        if (inters_x - wall[0][0]) * (inters_x - wall[1][0]) > 0 or (inters_y - wall[0][1]) * (
+                inters_y - wall[1][1]) > 0:
+            intersection_on_wall = False
+        # if theta_wall_move == 0:
+        #     safe_distance = 1000
+        #     print("12312")
+        # else:
+        #     safe_distance = abs(self.radius / np.sin(theta_wall_move * math.pi / 180))
+        robot_to_wall_point = min(((self.x - wall[0][0]) ** 2 + (self.y - wall[0][1]) ** 2) ** 0.5,
+                                  ((self.x - wall[1][0]) ** 2 + (self.y - wall[1][1]) ** 2) ** 0.5)
+        if towards_wall:
+            if intersection_on_wall:
+                if distance_vertical > closest_sensor:
+                    collision = True
             else:
-                parallel_x = -distance_parallel
-        elif 90 > theta_wall > 0:
-            if abs(theta_move) < 90:
-                parallel_x = -distance_parallel
-                parallel_y = -distance_parallel
+                if robot_to_wall_point < self.radius:
+                    collision = True
+        return collision
 
-        return parallel_x, parallel_y, distance_vertical, theta_wall
-
-    def parallelMove(self, distance_x, distance_y, theta_wall):
-        x = self.x
-        y = self.y
-        if theta_wall == 90:
-            y = self.y + distance_y
-        elif theta_wall == 0:
-            x = self.x + distance_x
-        else:
-            y = self.y + distance_y * abs(np.cos(theta_wall * math.pi / 180))
-            x = self.x + distance_x * abs(np.sin(theta_wall * math.pi / 180))
-        return x, y
-
-    def detectCollision(self, delta_t, next_x, next_y):
-        move = 0
-        if next_x == self.x:
-            move = abs(next_y - self.y)
-        elif next_y == self.y:
-            move = abs(next_x - self.x)
-        collision_dis = min(self.sensors)
-        collision = False
-        if collision_dis < move or collision_dis < 1:
-            collision = True
-            if delta_t > 0.1:
-                delta_t = delta_t * (collision_dis / move)
-        return delta_t, collision
-
-    def handleCollision(self, move_x, move_y, walls):
-        next_x = move_x
-        next_y = move_y
+    def detectCollision(self, move_x, move_y, walls):
         # get the closest distance
         sensors_copy = self.sensors.copy()
         sensors_copy.sort()
@@ -245,39 +206,7 @@ class Robot:
                 second_wall = walls[second_wall_index]
             else:
                 break
-        parallel_x_1, parallel_y_1, distance_vertical_first, theta_wall_first = \
-            self.decomposeMovement(next_x, next_y, first_wall)
-        parallel_x_2, parallel_y_2, distance_vertical_second, theta_wall_second = \
-            self.decomposeMovement(next_x, next_y, second_wall)
-        if collision_dis < abs(distance_vertical_first) and second_collision_dis < abs(distance_vertical_second):
-            if distance_vertical_first > 0 or distance_vertical_second > 0:
-                # stop at the corner
-                next_x = self.x
-                next_y = self.y
-        elif collision_dis < abs(distance_vertical_first) and 0 < distance_vertical_first:
-            if parallel_x_1 == 0.0:
-                # stop vertical to the wall
-                next_x = self.x
-                next_y = self.y
-            else:
-                next_x, next_y = self.parallelMove(parallel_x_1, parallel_y_1, theta_wall_first)
-
-        if next_x < 85 or next_x > 750 or next_y < 85 or next_y > 750:
-            next_x = self.x
-            next_y = self.y
-        return next_x, next_y
-
-    def get_intersection(self, point1, point2, point3, point4):
-        new_x, new_y = 10000, 10000
-        a1 = point2[1] - point1[1]
-        b1 = point1[0] - point2[0]
-        c1 = a1 * point1[0] + b1 * point1[1]
-        # Sensor line
-        a2 = point4[1] - point3[1]
-        b2 = point3[0] - point4[0]
-        c2 = a2 * point3[0] + b2 * point3[1]
-        determinant = a1 * b2 - a2 * b1
-        if abs(determinant) > 0.00000001:  # if there is an intersectioin
-            new_x = -(b1 * c2 - b2 * c1) / determinant  # intersection coordinate
-            new_y = (a1 * c2 - a2 * c1) / determinant
-        return new_x, new_y
+        collision1 = self.robot_and_wall(move_x, move_y, first_wall, collision_dis)
+        collision2 = self.robot_and_wall(move_x, move_y, second_wall, second_collision_dis)
+        collision = collision1 or collision2
+        return collision
--- run.py
+++ run.py
@@ -1,14 +1,22 @@
 import copy
 import math
-import numpy as np
 import pygame
 
 from Robot import Robot
 
-SEV = 35  # SCREEN_EDGE_VACANCY
-WIDTH = 835
-HEIGHT = 835
-
+sev = 35  # SCREEN_EDGE_VACANCY
+w = 900
+h = 900
+line0 = [(sev, sev), (sev, h - sev), (w - sev, h - sev), (w - sev, sev), (sev, sev)]
+line1 = [(300, sev), (300, 750)]
+line2 = [(600, sev), (600, 300)]
+line3 = [(600, 450), (600, h - sev)]
+room1 = [line0, line1, line2, line3]
+line_star = [(450, 50), (539, 327), (830, 326), (595, 496), (658, 773), (450, 602), (165, 773), (305, 496), (70, 326),
+             (360, 327), (450, 50)]
+star_room = [line_star]
+chosen_room = star_room
+initPosition = [400, 600]
 # A list of RGB values for the colours used in the game.
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -28,58 +36,46 @@ class Simulation:
 
     def __init__(self):
         pygame.init()
-        self.outer_wall = [(SEV, SEV), (SEV, WIDTH - SEV), (WIDTH - SEV, HEIGHT - SEV), (HEIGHT - SEV, SEV), (SEV, SEV)]
-        self.inner_wall = [(300, 400), (400, 500), (500, 400), (400, 300), (300, 400)]
-        self.robot = Robot(self.outer_wall, self.inner_wall, 100)
-        self.screen = pygame.display.set_mode((WIDTH + 350, HEIGHT))
+        self.robot = Robot(chosen_room, initPosition, 60)
+        self.screen = pygame.display.set_mode((w, h))
+
+        self.Sur = pygame.Surface
         pygame.display.set_caption("Modular Robot Simulator")
         self.font = pygame.font.SysFont("Pokemon GB.ttf", 50)
         self.sensor_font = pygame.font.SysFont("Pokemon GB.ttf", 20)
         self.running = True
         self.clock = pygame.time.Clock()
+        self.dust = []
 
     def show(self, keys, velocities):
         self.screen.fill(WHITE)
-
         # Fill in robot environment
-        pygame.draw.aalines(self.screen, DARKGRAY, True, self.outer_wall, 50)
-        pygame.draw.aalines(self.screen, DARKGRAY, True, self.inner_wall, 50)
-        pygame.draw.circle(self.screen, PURPLE, (self.robot.x, self.robot.y), 50)
+        for wall in chosen_room:
+            pygame.draw.aalines(self.screen, DARKGRAY, True, wall, 50)
+        # Display robot
+        rect = pygame.draw.circle(self.screen, PURPLE, (self.robot.x, self.robot.y), 30)
+        self.dust.append(rect)
+        # Display robot direction
         pygame.draw.line(self.screen, BLACK, (self.robot.x, self.robot.y), (self.robot.frontX, self.robot.frontY), 1)
-
-        # display sensors
+        # Display robot angle
+        angle_x = (self.robot.x + self.robot.frontX) / 2 - 5
+        angle_y = (self.robot.y + self.robot.frontY) / 2 - 5
+        self.screen.blit(self.sensor_font.render(str(round(velocities[2] % 360)), True, BLACK), (angle_x, angle_y))
+        # Display speed
+        left_x, left_y = self.robot.rotate(self.robot.theta + math.pi / 2, self.robot.radius - 10)
+        self.screen.blit(self.sensor_font.render(str(round(velocities[0])), True, BLACK), (left_x, left_y))
+        right_x, right_y = self.robot.rotate(self.robot.theta - math.pi / 2, self.robot.radius - 25)
+        self.screen.blit(self.sensor_font.render(str(round(velocities[1])), True, BLACK), (right_x, right_y))
+        self.screen.blit(self.sensor_font.render(velocities[4], True, BLACK), (5, 5))
+        # display  12 sensors
         angle = copy.copy(self.robot.theta)
         sensors = self.robot.sensors
         for i in range(12):
-            x, y = self.robot.rotate(angle, self.robot.radius + 12)
+            x, y = self.robot.rotate(angle, self.robot.radius + 15)
             x = x - 10
             y = y - 8
-            self.screen.blit(self.sensor_font.render(str(round(sensors[i])), True, BLACK), (x, y))
+            aa = self.screen.blit(self.sensor_font.render(str(round(sensors[i])), True, BLACK), (x, y))
             angle += math.pi / 6
-
-        # Fill in keys
-        pygame.draw.rect(self.screen, getColour(keys[0]), pygame.Rect(WIDTH + 50, HEIGHT / 2 - 100, 50, 50))
-        self.screen.blit(self.font.render("w", 105, WHITE), (WIDTH + 60, HEIGHT / 2 - 95))
-        pygame.draw.rect(self.screen, getColour(keys[1]), pygame.Rect(WIDTH + 50, HEIGHT / 2, 50, 50))
-        self.screen.blit(self.font.render("s", 105, WHITE), (WIDTH + 65, HEIGHT / 2 + 5))
-        pygame.draw.rect(self.screen, getColour(keys[5]), pygame.Rect(WIDTH + 150, HEIGHT / 2 - 100, 50, 50))
-        self.screen.blit(self.font.render("t", 105, WHITE), (WIDTH + 170, HEIGHT / 2 - 90))
-        pygame.draw.rect(self.screen, getColour(keys[6]), pygame.Rect(WIDTH + 150, HEIGHT / 2, 50, 50))
-        self.screen.blit(self.font.render("g", 105, WHITE), (WIDTH + 165, HEIGHT / 2 + 5))
-        pygame.draw.rect(self.screen, getColour(keys[2]), pygame.Rect(WIDTH + 250, HEIGHT / 2 - 100, 50, 50))
-        self.screen.blit(self.font.render("o", 105, WHITE), (WIDTH + 265, HEIGHT / 2 - 95))
-        pygame.draw.rect(self.screen, getColour(keys[3]), pygame.Rect(WIDTH + 250, HEIGHT / 2, 50, 50))
-        self.screen.blit(self.font.render("l", 105, WHITE), (WIDTH + 270, HEIGHT / 2 + 10))
-        pygame.draw.rect(self.screen, getColour(keys[4]), pygame.Rect(WIDTH + 150, HEIGHT / 2 + 100, 50, 50))
-        self.screen.blit(self.font.render("x", 105, WHITE), (WIDTH + 165, HEIGHT / 2 + 105))
-
-        # Display velocities
-        left = "V left = " + str(np.round(velocities[0],2))
-        self.screen.blit(self.font.render(left, 105, BLACK), (WIDTH + 50, HEIGHT / 2 - 305))
-        right = "V right = " + str(np.round(velocities[1],2))
-        self.screen.blit(self.font.render(right, 105, BLACK), (WIDTH + 50, HEIGHT / 2 - 255))
-        theta = "θ = " + str(np.round(velocities[2] % 360, 2)) + "°"
-        self.screen.blit(self.font.render(theta, 105, BLACK), (WIDTH + 50, HEIGHT / 2 - 205))
         pygame.display.flip()
 
     def run(self):
@@ -102,7 +98,7 @@ class Simulation:
         movement = [keys[pygame.K_w], keys[pygame.K_s], keys[pygame.K_o], keys[pygame.K_l], keys[pygame.K_x],
                     keys[pygame.K_t], keys[pygame.K_g]]
 
-        velocities = self.robot.move(movement, delta_t, self.outer_wall, self.inner_wall)
+        velocities = self.robot.move(movement, delta_t, chosen_room)
 
         return movement, velocities
 
