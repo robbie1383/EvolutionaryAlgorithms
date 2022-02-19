d0c4e300e9a55578fef5c5a9ca410c7392997016
Shahrzad Feyzi <florene.feyzi12@gmail.com>
2022-02-19 15:59:49 +0100

    new evaluation and selection functions

--- EA.py
+++ EA.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import random
 class EA:
 
     def __init__(self, size, r, optimization):
@@ -9,7 +9,9 @@ class EA:
 
     def evolve(self):
         evaluations = self.evaluate()
+        #evaluations = self.evaluateOther()
         selected = self.selection(evaluations)
+        #selected = self.selectionTournament(evaluations)
         children = self.crossover(selected)
         self.population = self.checkBounds(children)
         return np.min(evaluations), np.mean(evaluations)
@@ -19,6 +21,9 @@ class EA:
         evaluations = [self.optimization(particle[0], particle[1]) - minimum for particle in self.population]
         return evaluations
 
+    def evaluateOther(self):
+        return [self.optimization(particle[0], particle[1]) for particle in self.population]
+
     def selection(self, evaluations):
         chosen = 5
         sorted = evaluations.copy()
@@ -26,9 +31,23 @@ class EA:
         selected = []
         for i in range(chosen):
             selected.append(self.population[evaluations.index(sorted[i])])
+        print(len(selected))
+        return selected
+
+    def selectionTournament(self,evaluation,k=3):
+        selected=[]
+        best=random.randint(0,len(self.population)-1)
+        for i in range(5):
+            selection_ix = random.randint(0,len(self.population)-1)
+            # check if better (e.g. perform a tournament)
+            if evaluation[best] < evaluation[selection_ix]:
+                best = selection_ix
+            selected.append(self.population[selection_ix])
+        print(len(selected))
         return selected
 
     def reproduction(self, selected):
+
         children = [*selected, *selected]
         return children
 
@@ -39,6 +58,7 @@ class EA:
             children.append(selected[index])
             newChild = np.add(selected[index], (np.random.random(), np.random.random()))
             children.append(newChild)
+
         return children
 
     def checkBounds(self, children):
